fe18c0a5e6d429fdeeb46c4e3a3e2640ec289be3
Yuji Takaesu <taka.esu.user@gmail.com>
2017-07-02 13:18:27 +0900

    impliment base, exe/sgdoc and lib/sgdoc.rb

--- lib/sgdoc.rb
+++ lib/sgdoc.rb
@@ -1,5 +1,11 @@
-require "sgdoc/version"
+require 'aws-sdk'
+require 'erubis'
+require 'delegate'
 
-module Sgdoc
-  # Your code goes here...
-end
+require 'sgdoc/version'
+require 'sgdoc/cli'
+require 'sgdoc/client'
+require 'sgdoc/client_stub' # 消す
+require 'sgdoc/wrappers/security_group'
+require 'sgdoc/wrappers/ip_permission'
+require 'sgdoc/wrappers/ip_permission_collection'
