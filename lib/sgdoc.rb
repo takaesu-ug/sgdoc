7b84e532fa10671bde04e773e977252e7ba516b0
Yuji Takaesu <taka.esu.user@gmail.com>
2017-07-06 23:22:06 +0900

    todo comment

--- lib/sgdoc.rb
+++ lib/sgdoc.rb
@@ -5,7 +5,7 @@ require 'delegate'
 require 'sgdoc/version'
 require 'sgdoc/cli'
 require 'sgdoc/client'
-require 'sgdoc/client_stub' # 消す
+require 'sgdoc/client_stub' # TODO: 消す
 require 'sgdoc/wrappers/security_group'
 require 'sgdoc/wrappers/ip_permission'
 require 'sgdoc/wrappers/ip_permission_collection'
